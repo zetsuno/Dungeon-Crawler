b899f7b8fbab2b2601a23da4871d7bf560257319
zetsuno <daniel_alex_fernandes@hotmail.com>
2015-04-28 22:34:28 +0100

    Update Server.c
    Função de começar o jogo implementada

--- server.c
+++ server.c
@@ -11,19 +11,18 @@
 #include <signal.h>
 #include <unistd.h>
 #include <math.h>
-
+#define MAX 100
 
 
 
 typedef struct dados_players
 {
-    char nome[100];
     int saude; //start = 20, Max = 30
     int peso; // Max = 20
     int atk;
     int def;
     char inventory[20][20];
-    int coin_count = 0; //contador de moedas
+    int coin_count; //contador de moedas
 }jogador;
 
 
@@ -70,26 +69,26 @@ typedef struct dados_room
 
 sala labirinto[10][10];
 
-monstros tabela_monstros[5]
+monstros tabela_monstros[5] =
 {
  [0] = {"morcego", 1, 4, 3, 4, 4, 5, 10, 1, 1,"NULL" },
  [1] = {"escorpiao", 1, 7, 5, 7, 7, 9, 10, 1, 0, "moeda"},
  [2] = {"lobisomem", 5, 7, 5, 7, 7, 9, 4, 1, 1, "faca"},
- [3] = {"pedobear", 8, 10, 10, 12, 10, 2, 0, 0, "faca moeda"},  //quando é mais que 1 item, podemos usar o strtok pra dividir isto bem, e mais facilmente
- [4] = {"b0ss", 10, 12, 15, 15, 15, 15, 1, 0, 0, "moeda moeda moeda moeda moeda"};
-}
+ [3] = {"pedobear", 8, 10, 10, 12, 10, 10, 2, 0, 0, "faca moeda"},  //quando é mais que 1 item, podemos usar o strtok pra dividir isto bem, e mais facilmente
+ [4] = {"b0ss", 10, 12, 15, 15, 15, 15, 1, 0, 0, "moeda moeda moeda moeda moeda"}
+};
 
-items tabela_items[8]
+items tabela_items[8] =
 {
- [0] = {"sandes", 0.5, 10, 0, 0, 1, 3, NULL},
- [1] = {"aspirina", 0.1, 20, 0, 0, 1, 1, NULL},
- [2] = {"xarope", 1.0, 4, 0, 0, 1, 4, NULL},
- [3] = {"faca", 2.0, 5, 5, 0, NULL, NULL, NULL},
- [4] = {"espada", 8.0, 3, 8, 2, NULL, NULL, 2},
- [5] = {"granada", 1.0, 2, 30, 0, 1, -5, NULL},
- [6] = {"escudo", 4.0, 4, 5, 5, NULL, NULL, 5},
- [7] = {"moeda", 0.1, 5, 0, 0, NULL, NULL, NULL};
-}
+ [0] = {"sandes", 0.5, 10, 0, 0, 1, 3, -99},   //Assumir que -99 = NULL
+ [1] = {"aspirina", 0.1, 20, 0, 0, 1, 1, -99},
+ [2] = {"xarope", 1.0, 4, 0, 0, 1, 4,-99},
+ [3] = {"faca", 2.0, 5, 5, 0, -99, -99, -99},
+ [4] = {"espada", 8.0, 3, 8, 2, -99, -99, 2},
+ [5] = {"granada", 1.0, 2, 30, 0, 1, -5, -99},
+ [6] = {"escudo", 4.0, 4, 5, 5, -99, -99, 5},
+ [7] = {"moeda", 0.1, 5, 0, 0, -99, -99, -99}
+};
 
 jogador lista_jogadores[10];
 
@@ -110,14 +109,69 @@ int random_number(int min_num, int max_num)   //genera valor aleatorio entre min
 }
 
 
+int avalia_frase(char **palavra, int aux)
+{
+    int i = 0, timeout_aux;
+    char *str_aux;
+    FILE *f;
 
+    if(strcmp(palavra[i],"novo") == 0){
+        i++;
+        timeout_aux = atoi(palavra[i]);
+        if(timeout_aux<0){
+            printf("[ERRO]: Timeout Invalido!\n");
+            return -1;
+        }
+        i++;
+        str_aux = palavra[i];
+        if(i >= aux)
+            return;
+        else{
+        f=fopen(str_aux, "rt");
+        if(f == NULL){
+            printf("[ERRO]: Erro ao abrir o ficheiro '%s'\n", str_aux);
+            }
+        }
 
-
-void main(){
-
-
-
-
+    }
 }
 
+void help(){
+system("clear");
+printf("----- Help -----\n\n");
+printf(" >  novo {valor-timeout} {nome-ficheiro}  -- Comecar um jogo novo a partir de um ficheiro \n");
+printf(" >  novo {valor-timeout} {valor-dificuldade} -- Comecar o jogo com distribuição aleatória \n");
+;
+}
+int main(){
+/////////////Client.c UI - AQUI PARA TESTES APENAS //////////////
+char str[MAX];
+char *palavra[8];
+int i=0, res, estado;
 
+do{
+printf(">>>");
+scanf(" %[^\n]", str);
+if(strcmp(str, "help")==0)
+{
+    help();
+}
+palavra[i] = strtok(str, " ");
+do{
+	i++;
+	palavra[i] = strtok(NULL," ");
+}while(palavra[i] != NULL);
+
+	res = avalia_frase(palavra, i);
+	if(res == 1){
+	printf("[ERRO]: Comando nao reconhecido!\n");
+	}
+	if(res == -1){
+	printf("[ERRO]: Sintaxe do comando inválida!\n");
+    }
+
+
+}while(strcmp(str,"fim") !=0);
+
+return 1;
+}
